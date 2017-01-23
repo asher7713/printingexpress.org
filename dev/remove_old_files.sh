ffaceb99e7e7480b191f730541f4749dee74ac75
Brian Hazeltine <brian@wasthat.me>
2017-01-23 16:11:48 -0600

    Remove old files on new deploys

---
+++ dev/remove_old_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Remove any files that are still in s3
+# but not in the dist directory
+
+set -ev
+
+if [[ $BRANCH -eq "master" ]]; then
+    BUCKET=$PROD_BUCKET
+elif [[ $BRANCH -eq "develop" ]]; then
+    BUCKET=$DEV_BUCKET
+else
+    exit 1
+fi
+
+for f in `aws s3 ls --recursive $BUCKET | awk '{print $4}'`
+    if [ ! -f dist/$f ]; then
+        aws s3 rm s3://$BUCKET/$f
+    fi
