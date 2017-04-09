c7432c8b7368e6ac10d6ce241e16ac869f5e1232
Brian Hazeltine <onwsk8r@users.noreply.github.com>
2017-04-09 14:37:09 -0500

    merge hotfix/window (#18)
    * Update to Angular 4
    
    * Fix animations
    
    * Remove reference to window
    
    * Add missing semicolon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, ValueProvider } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -27,6 +28,7 @@ const WINDOW_PROVIDER: ValueProvider = {
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         HttpModule,
         AppRoutingModule,
